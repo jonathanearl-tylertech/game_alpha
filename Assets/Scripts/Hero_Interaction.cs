1776943a5b0a350b874ce896897f1d371a439a69
Jonathan Earl <metablue@uw.edu>
2016-05-14 12:39:40 -0700

    Cleaned up meemo

--- Assets/Scripts/Hero_Interaction.cs
+++ Assets/Scripts/Hero_Interaction.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Hero_Interaction : MonoBehaviour {
 	private Canvas gameOverCanvas;
+	CameraBehavior globalBehavior;
 
     public float max_speed = 2f;
     public float air_speed = 0.1f;
@@ -36,8 +37,19 @@ public class Hero_Interaction : MonoBehaviour {
 	private bool is_using_power = false;
 	#endregion
 
+	#region meemostate support
+	public enum MeemoState
+	{
+		Normal,
+		Bubble,
+		Hurt
+	}
+	public MeemoState current_state;
+	#endregion
+
 	// Use this for initialization
 	void Start () {
+		this.globalBehavior = GameObject.Find("Main Camera").GetComponent<CameraBehavior>();
 		mSize = GetComponent<Renderer> ().bounds.size;
 		this.health_bar = GameObject.Find ("HealthBar").GetComponent<HealthBar_interaction> ();
         this.rigid_body = this.GetComponent<Rigidbody2D>();
@@ -46,90 +58,49 @@ public class Hero_Interaction : MonoBehaviour {
 		this.star_bar = GameObject.Find ("StarBar").GetComponent<StarBar_interaction> ();
 		gameOverCanvas = GameObject.Find ("GameOverCanvas").GetComponent<Canvas> ();
 		gameOverCanvas.enabled = false;		// The GameOverCanvas has to be initially enabled on the Unity UI
+		current_state = MeemoState.Normal;
     }
 
     void FixedUpdate () {
-		// Handle when hero collided with the bottom bound of the window (die)
-		CameraBehavior globalBehavior = GameObject.Find("Main Camera").GetComponent<CameraBehavior>();
-		CameraBehavior.WorldBoundStatus status = globalBehavior.ObjectCollideWorldBound (GetComponent<Renderer> ().bounds);
-		Vector3 pos = globalBehavior.mCamera.WorldToViewportPoint (transform.position);
-		Vector3 backgroundSize = GameObject.Find ("backgroundImage").GetComponent<Renderer> ().bounds.size;
-		pos.x = Mathf.Clamp (pos.x, 0.03f, 1f - (mSize.x / backgroundSize.x)); //(1f / backgroundSize.x * mSize.x / 2f));
-		pos.y = Mathf.Clamp (pos.y, 0.035f, 1f - (mSize.y / backgroundSize.y));
-		transform.position = globalBehavior.mCamera.ViewportToWorldPoint (pos);
-
-
-		//if (status == CameraBehavior.WorldBoundStatus.CollideBottom) {
-		if (transform.position.y - mSize.y/2f <= globalBehavior.globalyMin)
-		{
-			// Destroy Meemo
-			// TimeScale = 0;
-			// Panel is active
-			Destroy (GameObject.Find("Meemo"));
-			Time.timeScale = 0;
-			//GameObject.Find ("GameManager").GetComponent<GlobalBehaviour>().enabled = false; // SetActive to false to stop generating new bubble
-			//GameObject.Find("Bubble(Clone)").GetComponent<BubbleBehaviour>().enabled = false;
-			gameOverCanvas.enabled = true;
-		}
-
-
+		this.change_direction ();
 		/// Interaction with bubble
-		if (isInBubble) {
-			//Debug.Log ("InBubble");
+		switch (this.current_state) {
+			case MeemoState.Bubble:
+				if (Input.GetAxis ("Horizontal") != 0f) { // When meemo is controlling the horizontal direction
+					this.move_in_bubble ();
+				} else { // When meemo is following bubble
+					this.follow_in_bubble ();
+				}
+				break;
+			case MeemoState.Normal:
+				this.grounded = Physics2D.OverlapCircle (this.ground_check.position, this.ground_radius, this.what_is_ground);
+				this.rigid_body.velocity = new Vector2 (Input.GetAxis ("Horizontal") * max_speed, this.rigid_body.velocity.y);
 
-			if (Input.GetAxis ("Horizontal") != 0f) { // When meemo is controlling the horizontal direction
-				
-
-				Debug.Log("MOVING IN BUBBLE");
-				float bnewY = bubble.transform.position.y + 0.01f;// bubble floats
-				float bnewX = transform.position.x + Input.GetAxis ("Horizontal") * (air_speed);
-				bubble.transform.position = new Vector3 (bnewX, bnewY, 0f);
-				bubble.initpos.x = bnewX;
-
-				transform.position = new Vector3 (bnewX, bnewY - 0.2f, transform.position.z);
-
-			} else { // When meemo is following bubble
-				Debug.Log("Not moving in bubble");
-				FollowSineCurve();
-
-				// update meemo's position to bubble'e sine curve
-				transform.position = new Vector3 (bubble.transform.position.x - 0.05f,
-					bubble.transform.position.y - GetComponent<Renderer> ().bounds.size.y / 2f + 0.5f, bubble.transform.position.z);
-			}
-
-		} else {
-
-			//Debug.Log ("Not in bubble!");
-			this.grounded = Physics2D.OverlapCircle(this.ground_check.position, this.ground_radius, this.what_is_ground);
-			this.rigid_body.velocity = new Vector2(Input.GetAxis("Horizontal") * max_speed, this.rigid_body.velocity.y);
-
-			if (Input.GetKeyDown ("space") && this.grounded) {
-				Jump ();
-			} else if (Input.GetKeyDown ("space") && !this.grounded && this.star_timer > 0f) {
-				is_using_power = true;
-			}
-			if (!Input.GetKey ("space") || this.star_timer <= 0f) {
-				is_using_power = false;
-			}
-			if (is_using_power) {
-				fly ();
-			}
+				if (is_using_power) {
+					fly ();
+				}
+				break;
 		}
-
+		this.ClampToCamera ();
+		this.CheckDeath ();
 		/// End interaction with bubble
-
-		if (Input.GetAxis ("Horizontal") < 0f && isFacingRight) {
-			transform.localScale = new Vector3 (-.3f, .3f, 1f);
-			isFacingRight = false;
-		}
-
-		if (Input.GetAxis ("Horizontal") > 0f && !isFacingRight) {
-			transform.localScale = new Vector3 (.3f, .3f, 1f);
-			isFacingRight = true;
-		}
-
     }
 
+	void Update() {
+		if (Input.GetKey ("space") && !this.grounded && this.star_timer > 0f) {
+			is_using_power = true;
+		}
+		else {
+			is_using_power = false;
+		}
+	}
+		
+	// Currently unused
+//    void Jump ()
+//    {
+//        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10f), ForceMode2D.Impulse);
+//    }
+	#region starpower support
 	void fly () {
 		this.star_timer -= Time.fixedDeltaTime;
 		this.rigid_body.AddForce (new Vector2 (5f, 20f), ForceMode2D.Force);
@@ -140,12 +111,23 @@ public class Hero_Interaction : MonoBehaviour {
 		this.star_timer = 1f;
 		star_bar.UpdateStarBarSize (this.star_timer);
 	}
+	#endregion
 
-    void Jump ()
-    {
-        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10f), ForceMode2D.Impulse);
-    }
+	#region direction support
+	private void change_direction() {
+		if (Input.GetAxis ("Horizontal") < 0f && isFacingRight) {
+			transform.localScale = new Vector3 (-.3f, .3f, 1f);
+			isFacingRight = false;
+		}
 
+		if (Input.GetAxis ("Horizontal") > 0f && !isFacingRight) {
+			transform.localScale = new Vector3 (.3f, .3f, 1f);
+			isFacingRight = true;
+		}
+	}
+	#endregion
+
+	#region bubble support
 	// Update position of bubble following sine curve
 	private void FollowSineCurve(){
 		float newY = bubble.transform.position.y + 0.03f;
@@ -159,5 +141,56 @@ public class Hero_Interaction : MonoBehaviour {
 		float sinFreqScale = bubble.sinOsc * 2f * (Mathf.PI) / globalBehaviour.WorldMax.y;
 		return bubble.sinAmp * (Mathf.Sin(y * sinFreqScale));
 	}
+	#endregion
 
+	#region Bubble support
+	private void move_in_bubble() {
+		float bnewY = bubble.transform.position.y + 0.03f;// bubble floats
+		float bnewX = transform.position.x + Input.GetAxis ("Horizontal") * (air_speed);
+		bubble.transform.position = new Vector3 (bnewX, bnewY, 0f);
+		bubble.initpos.x = bnewX;
+
+		transform.position = new Vector3 (bnewX, bnewY - 0.2f, transform.position.z);
+	}
+
+	private void follow_in_bubble() {
+		FollowSineCurve();
+		// update meemo's position to bubble'e sine curve
+		transform.position = new Vector3 (bubble.transform.position.x - 0.05f,
+			bubble.transform.position.y - GetComponent<Renderer> ().bounds.size.y / 2f + 0.5f, bubble.transform.position.z);
+	}
+	#endregion
+
+	#region camera support
+	private void ClampToCamera() {
+		// Handle when hero collided with the bottom bound of the window (die)
+		CameraBehavior.WorldBoundStatus status = globalBehavior.ObjectCollideWorldBound (GetComponent<Renderer> ().bounds);
+		Vector3 pos = globalBehavior.mCamera.WorldToViewportPoint (transform.position);
+		Vector3 backgroundSize = GameObject.Find ("backgroundImage").GetComponent<Renderer> ().bounds.size;
+		pos.x = Mathf.Clamp (pos.x, 0.03f, 1f - (mSize.x / backgroundSize.x)); //(1f / backgroundSize.x * mSize.x / 2f));
+		pos.y = Mathf.Clamp (pos.y, 0.035f, 1f - (mSize.y / backgroundSize.y));
+		transform.position = globalBehavior.mCamera.ViewportToWorldPoint (pos);
+		if (transform.position.y - mSize.y/2f <= globalBehavior.globalyMin)
+		{
+			// Destroy Meemo
+			// TimeScale = 0;
+			// Panel is active
+			Destroy (GameObject.Find("Meemo"));
+			Time.timeScale = 0;
+			gameOverCanvas.enabled = true;
+		}
+	}
+
+	private void CheckDeath() {
+		if (transform.position.y - mSize.y/2f <= globalBehavior.globalyMin)
+		{
+			// Destroy Meemo
+			// TimeScale = 0;
+			// Panel is active
+			Destroy (GameObject.Find("Meemo"));
+			Time.timeScale = 0;
+			gameOverCanvas.enabled = true;
+		}
+	}
+	#endregion
 }
