87b78f7378ccf0bb367fc60bfffcc130cdb8576f
Diem To <diemto92@gmail.com>
2016-05-17 20:26:47 -0700

    Added Puffer

--- Assets/Scripts/PufferBehavior.cs
+++ Assets/Scripts/PufferBehavior.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class PufferBehavior : MonoBehaviour {
-	private float distFromMeemoToActivateTrigger = 5f;
+	private float distFromMeemoToActivateTrigger = 10f;
+	private float timer = -1.0f;
+	private const float MAX_TIME = 3.0f;
+	public Animator anim;
 
 	#region state support
 	public enum PufferState
@@ -16,18 +19,44 @@ public class PufferBehavior : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		currentState = PufferState.Little;
+		anim = GetComponentInParent<Animator> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		Canvas gameOverCanvas = GameObject.Find ("GameOverCanvas").GetComponent<Canvas> ();
-		if (currentState == PufferState.Little && !gameOverCanvas.enabled) {
+		if (!gameOverCanvas.enabled) {
 			Hero_Interaction meemo = GameObject.FindGameObjectWithTag ("Player").GetComponent<Hero_Interaction> ();
-			if (Vector3.Distance (meemo.transform.position, transform.position) < distFromMeemoToActivateTrigger) {
-				currentState = PufferState.Puffed;
+			switch (currentState) {
+			case PufferState.Little:
+				if (Vector3.Distance (meemo.transform.position, transform.position) < distFromMeemoToActivateTrigger) {
+					currentState = PufferState.Puffed;
+					Debug.Log (currentState);
+					anim.SetBool ("bool",true);
+				}
+				break;
+			case PufferState.Puffed:
+				Debug.Log (timer);
+				if (Vector3.Distance (meemo.transform.position, transform.position) < distFromMeemoToActivateTrigger) {
+					if (timer != -1.0f) {
+						timer = 0.0f;
+					}
+				} else {
+					if (timer == -1.0f) {
+						timer = 0.0f;
+					} else {
+						if (timer >= MAX_TIME) {
+							currentState = PufferState.Little;
+							Debug.Log (currentState);
+							anim.SetBool ("bool",false);
+						} else {
+							timer += Time.deltaTime;
+
+						}
+					}
+				}
+				break;
 			}
-		} else {
-			
 		}
 	}
 }
