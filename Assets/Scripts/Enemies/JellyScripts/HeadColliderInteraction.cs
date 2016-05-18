ca3934da915ff7658311c36a2edc3ec12a0f310c
Diem To <diemto92@gmail.com>
2016-05-18 11:20:08 -0700

    Update?

--- Assets/Scripts/Enemies/JellyScripts/HeadColliderInteraction.cs
+++ Assets/Scripts/Enemies/JellyScripts/HeadColliderInteraction.cs
@@ -21,7 +21,12 @@ public class HeadColliderInteraction : MonoBehaviour {
 	{
 		if (other.gameObject.tag == "Player")
 		{
+			//play meemo getting squished animation
+			Animator meemoAnim = other.gameObject.GetComponent<Animator>();
+			meemoAnim.SetTrigger ("squishMeemo");
+
 			anim.SetTrigger ("trigger");
+
 			Rigidbody2D hero_rigid = other.gameObject.GetComponent<Rigidbody2D>();
 			hero_rigid.velocity = new Vector3(hero_rigid.velocity.x, 0f, 0f);
 			// choose up or down bounce
@@ -29,6 +34,8 @@ public class HeadColliderInteraction : MonoBehaviour {
 			if (other.gameObject.transform.position.y > this.transform.position.y) direction = 1;
 			else direction = -1;
 			hero_rigid.AddForce(new Vector3(hero_rigid.velocity.x, direction * bounce_force, 0), ForceMode2D.Impulse);
+
+
 		}
 	}
 }
