cbec207e051220681e8db24adfc328dcda09a773
Diem To <diemto92@gmail.com>
2016-06-01 19:18:40 -0700

    update tutorial bubbles

--- Assets/Scripts/UI/TutorialBubbleBehavior.cs
+++ Assets/Scripts/UI/TutorialBubbleBehavior.cs
@@ -2,29 +2,80 @@
 using System.Collections;
 
 public class TutorialBubbleBehavior : MonoBehaviour {
-	private TuBuState State;
+	public static float sinAmp = 0.75f;
+	public static float sinOsc = 45f;
+	public static float bubbleSpeed = 3f;
+	private Vector3 bgSize;
 
-	private enum TuBuState
+	public float distance = 1f;
+	public float speed = 1f;
+	public Vector3 initpos;
+	private float maxY;
+	private float minY;
+	private bool isMovingUp;
+
+	private TBState State;
+	private enum TBState
 	{
 		Station,
-		Moving
+		Floating
 	}
 	// Use this for initialization
 	void Start () {
-		State = TuBuState.Station;
+		bgSize = GameObject.Find ("backgroundImage").GetComponent<Renderer> ().bounds.size;
+		State = TBState.Station;
+		initpos = transform.position;
+		maxY = initpos.y + distance;
+		minY = initpos.y - distance;
+		isMovingUp = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		switch (State) {
+		case TBState.Station:
+			float currentY = transform.position.y; 
+			if (currentY > maxY) {
+				isMovingUp = false;
+			} else if (currentY < minY) {
+				isMovingUp = true;
+			}
+			if (isMovingUp) {
+				MoveUp ();
+			} else {
+				MoveDown ();
+			}
+			break;
+		case TBState.Floating:
+			FollowSineCurve ();
+			break;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
-			
-
+			State = TBState.Floating;
 		}
+	}
+
+	private void MoveUp(){
+		transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime, transform.position.z);
+	}
+
+	private void MoveDown(){
+		transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
+	}
+
+	public void FollowSineCurve(){
+		float newY = transform.position.y + bubbleSpeed * Time.deltaTime;
+		float newX = initpos.x + GetXValue (newY); 
+		transform.position = new Vector3 (newX, newY, 0f);
+	}
+
+	private float GetXValue(float y){
+		float sinFreqScale = sinOsc * 2f * (Mathf.PI) / bgSize.y;
+		return sinAmp * (Mathf.Sin(y * sinFreqScale));
 	}
 }
