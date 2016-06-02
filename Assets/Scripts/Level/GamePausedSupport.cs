3f7b259a76f46285a4135833d76a231a78818fa1
Vuochly Ky <ninvk@uw.edu>
2016-06-02 12:42:02 -0700

    add Paused function and small fixed on menu scene
    Plus added bubble particle system to menu scene

---
+++ Assets/Scripts/Level/GamePausedSupport.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GamePausedSupport : MonoBehaviour
+{
+    public GameObject PausedPanel;
+    private bool escPressed = false;
+    public Button mResume;
+    public Button mRestart;
+    public Button mExit;
+
+    // Use this for initialization
+    void Start()
+    {
+        PausedPanel.SetActive(false);
+        // Workflow assume:
+        //      mLevelOneButton: is dragged/placed from UI
+        // add in listener
+        mRestart.onClick.AddListener(
+            () => {                     // Lamda operator: define an annoymous function
+                LoadScene(SceneManager.GetActiveScene().name);
+                Time.timeScale = 1;
+            });
+        mResume.onClick.AddListener(
+            () => {                     // Lamda operator: define an annoymous function
+                PausedPanel.SetActive(false);
+                Time.timeScale = 1;
+            });
+        mExit.onClick.AddListener(
+            () => {                     // Lamda operator: define an annoymous function
+                Application.Quit();
+            });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Canvas gameOverCanvas = GameObject.Find("GameOverCanvas").GetComponent<Canvas>();
+        if (!gameOverCanvas.enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                escPressed = !escPressed;
+                if (escPressed)
+                {
+                    PausedPanel.SetActive(true);
+                    Time.timeScale = 0;
+                }
+                else
+                {
+                    PausedPanel.SetActive(false);
+                    Time.timeScale = 1;
+                }
+            }
+        }
+
+    }
+
+    void LoadScene(string theLevel)
+    {
+        SceneManager.LoadScene(theLevel);
+    }
+}
